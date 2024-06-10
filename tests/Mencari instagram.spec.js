a0d7f9529330638bcc53f988735e6057ec45a4d9
agil <Agi@indonesiaindicator.com>
2024-06-10 16:25:30 +0700

    update code

--- tests/Mencari instagram.spec.js
+++ tests/Mencari instagram.spec.js
@@ -1,6 +1,16 @@
 const { test, expect } = require('@playwright/test');
+const fs = require('fs');
+const path = require('path');
 
 test('Mencari akun Instagram dan mengambil screenshot postingan pertama', async ({ page }) => {
+  // Nama folder untuk menyimpan screenshot
+  const screenshotDir = path.join('d', 'screenshots');
+
+  // Buat folder jika belum ada
+  if (!fs.existsSync(screenshotDir)) {
+    fs.mkdirSync(screenshotDir, { recursive: true });
+  }
+
   // Buka situs Instagram
   await page.goto('https://www.instagram.com/', { waitUntil: 'networkidle' });
 
@@ -8,29 +18,29 @@ test('Mencari akun Instagram dan mengambil screenshot postingan pertama', async 
   await page.waitForSelector('input[name="username"]');
 
   // Masukkan username dan password Instagram Anda
-  await page.fill('input[name="username"]', 'agilajahdah');
-  await page.fill('input[name="password"]', 'Agilajah01');
+  await page.fill('input[name="username"]', 'yogayunand4');
+  await page.fill('input[name="password"]', 'bakwanrebus01!');
   await page.click('button[type="submit"]');
 
   // Tunggu hingga halaman utama muncul
   await page.waitForNavigation({ waitUntil: 'networkidle' });
 
-  // Buka halaman profil pengguna
-  await page.goto('https://www.instagram.com/alwiassegaf03/', { waitUntil: 'networkidle' });
+  // Beri jeda untuk memastikan halaman sepenuhnya dimuat
+  await page.waitForTimeout(3000);
 
-  // Tunggu hingga postingan pertama muncul
-  /*await page.waitForSelector('article > div img');*/
+  // Buka halaman profil pengguna
+  await page.goto('https://www.instagram.com/alwiassegaf03/');
+  /*// Tunggu hingga postingan pertama muncul
+  await page.waitForSelector('article > div img');*/
 
   // Pilih postingan pertama
-  const firstPost = await page.$('article > div img');
-  await firstPost.click();
+  await page.goto('https://www.instagram.com/p/C78Xn_wvcIt/');
+  await page.waitForTimeout(3000);
 
-  // Tunggu hingga postingan dimuat
-  await page.waitForSelector('div[role="dialog"] img');
 
   // Ambil screenshot dari postingan pertama
-  await page.screenshot({ path: 'first_post.png', fullPage: true });
+  const screenshotPath = path.join(screenshotDir, 'first_post.png');
+  await page.screenshot({ path: screenshotPath, fullPage: true });
 
-  // Verifikasi screenshot diambil dengan benar
-  expect(await page.screenshot({ path: 'first_post.png', fullPage: true })).toBeTruthy();
+  console.log(`Screenshot berhasil diambil: ${screenshotPath}`);
 });
